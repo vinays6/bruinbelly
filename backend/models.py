aa62d4a8e23d0a1d5766174ee44bc8ae18647d0e
Vinay S <73406313+vinays6@users.noreply.github.com>
2026-03-01 15:24:14 -0800

    fake auth

--- backend/models.py
+++ backend/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
 
     reviews = db.relationship(
         'Review',
